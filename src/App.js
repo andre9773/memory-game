0e1fe9b1fa92ae7e4e696c2fd7dc15263fbdf853
Andrii Terytsia <a-aterytsia@microsoft.com>
2022-11-28 17:36:15 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import './App.css';
+import { useState } from 'react';
+import SinglCard from './components/SinglCard';
+
+
  const cardImages = [
-  {"src": "/img/helmet-1.png"},
-  {"src": "/img/potion-1.png"},
-  {"src": "/img/ring-1.png"},
-  {"src": "/img/scroll-1.png"},
-  {"src": "/img/shield-1.png"},
-  {"src": "/img/sword-1.png"}
+  {"src": "/img/1.png"},
+  {"src": "/img/2.png"},
+  {"src": "/img/3.png"},
+  {"src": "/img/4.png"},
+  {"src": "/img/5.png"},
+  {"src": "/img/6.png"}
  ]
 function App() {
-  
-
+  const [cards, setCards] = useState([]);
+  const [choiceOne, setChoiseOne] = useState(null)
+  const [choiceTwo, setChoiseTwo] = useState(null)
   // shuffle cards
 const shuffleCards = () =>{
   const shuffledCards = [...cardImages, ...cardImages]
   .sort(() => Math.random() - 0.5)
   .map((card) => ({...card, id: Math.random()}))
+
+  setCards(shuffledCards)
 }
+
 
   return (
     <div className="App">
-      <h1>Magic Match</h1>
-      <button>New Game</button>
+      <h1>Memory Game </h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SinglCard src={card.src} key={card.id}/>
+        ))}
+      </div>
     </div>
   );
 }
